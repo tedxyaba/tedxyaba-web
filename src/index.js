767db7cea41c4c5de235953e299da8a358804fa8
ceemion <fiyinsimeon@gmail.com>
2019-07-04 11:05:18 +0100

    Add and setup Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// These can be imported and used, commenting out till when needed.
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
