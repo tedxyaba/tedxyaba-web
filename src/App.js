b1446cc94c605055b3c108aaeb605c5fdbd8a644
ceemion <fiyinsimeon@gmail.com>
2019-07-04 11:15:18 +0100

    Add navbar structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 // load pages
 import Home from '../src/pages/Home';
 
+// load components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.scss';
+
+const Navbar = (props) => {
+  const title = props.title || 'Tedx Yaba';
+
+  return (
+    <div>
+      <div>{title}</div>
+      <div>
+        <a href="/">Home</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
