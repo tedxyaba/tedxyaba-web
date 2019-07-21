0e6fb636aeba77a7cb06268e094ef563390dbd60
Sunday Adefila <adefilaedward@gmail.com>
2019-07-21 15:07:28 +0100

    group events by year

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -11,7 +11,7 @@ class Events extends Component {
 
     this.state = {
       loading: true,
-      data: [],
+      data: {},
       errors: null
     }
 
@@ -22,13 +22,23 @@ class Events extends Component {
     this._fetchData()
   }
 
+  groupByYear(events) {
+    const reducer = (h, event) => {
+      const year = moment(event.eventDate).format('YYYY')
+      h[year] = (h[year] || []).concat(event);
+      return h;
+    }
+    return events.reduce(reducer, {})
+  }
+
   _fetchData() {
     const apRoute = apiRoutes.events();
     const cb = (success, data) => {
       if (success) {
+        const serialized = TransformEventsListData(data)
         this.setState({
           loading: false,
-          data: TransformEventsListData(data)
+          data: this.groupByYear(serialized)
         })
       } else {
         this.setState({
@@ -42,23 +52,41 @@ class Events extends Component {
 
   render() {
     const { loading, data } = this.state;
-    const EventBox = (props) => {
+    const EventBox = (event) => {
       return (
-        <div className="px-3 py-5 col-md-6 text-truncate">
+        <div className="px-3 pb-5 col-md-6 text-truncate">
           <div className="card">
             <div className="card-body row">
               <div className="col-4">
-                <img src={props.image.thumbnail_list_url} className="card-img-top" alt={props.image.alt} />
+                <img src={event.image.thumbnail_list_url} className="card-img-top" alt={event.image.alt} />
               </div>
               <div className="col-8 text-truncate">
-                <h4 className="card-title">{ props.title }</h4>
-                <p className="event-date">{ moment(props.eventDate).format('Do MMMM YYYY') }</p>
-                <small className="text-muted">{ props.summary }</small>
+                <h4 className="card-title">{ event.title }</h4>
+                <p className="event-date">{ moment(event.eventDate).format('Do MMMM YYYY') }</p>
+                <small className="text-muted">{ event.summary }</small>
                 <div className="event-cta">
                   <a href={''} target={''} className="btn btn-primary">View Details</a>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      )
+    }
+
+    const YearlyEvents = (props) => {
+      return (
+        <div>
+          <h5 className='px-3 font-weight-bold' style={{overflow: 'hidden', whiteSpace: 'nowrap'}}>
+            {props.year}
+            <hr className='my-1' style={{display: 'inline-block', width: '100%'}} />
+          </h5>
+          <div className="row">
+            {
+              props.events.map((event, index) => {
+                return <EventBox key={index} {...event} />
+              })
+            }
           </div>
         </div>
       )
@@ -70,10 +98,10 @@ class Events extends Component {
           // replace this with the new loading component
           <div className="my-4 text-center">Loading...</div>
         ) : (
-          <div className="row">
+          <div>
             {
-              data.map((event, index) => {
-                return <EventBox key={index} {...event} />
+              Object.keys(data).map( year => {
+                return <YearlyEvents key={year} year={year} events={data[year]} />
               })
             }
           </div>
