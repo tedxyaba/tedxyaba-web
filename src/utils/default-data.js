6deda4efc38afb8bc56a79b4cc6bf5d2482a77da
ceemion <fiyinsimeon@gmail.com>
2020-04-01 16:52:08 +0100

    Add default data file.

---
+++ src/utils/default-data.js
@@ -0,0 +1,98 @@
+const about = [
+  {
+    id: 'ted',
+    title: 'What is TED?',
+    content: 'TED is a nonprofit organization devoted to Ideas Worth Spreading, usually in the form of short, powerful talks (18 minutes or fewer) delivered by today’s leading thinkers and doers.\n\nMany of these talks are given at TED’s annual conference in Vancouver, British Columbia, and made available, free, on TED.com. TED speakers have included Bill Gates, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson, Monica Lewinsky, Philippe Starck, Ngozi Okonjo-Iweala, Sal Khan and Daniel Kahneman.'
+  },
+  {
+    id: 'tedx',
+    title: 'What is TEDx?',
+    content: 'In the spirit of “ideas worth spreading,” TED has created TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience.\n\nOur event is called TEDxYaba, where x = independently organized TED event. At TEDxYaba, TED Talks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.'
+  },
+  {
+    id: 'tedxyaba',
+    title: 'What is TEDxYaba?',
+    content: 'TEDxYaba is an independently organized TED event in the heart of Yaba, Lagos, the centre of Africa’s largest technology ecosystem.\n\nOur vision is to be the foremost platform for African ideas on the continent and, every year, we curate conversations that we hope would catalyse and inspire further corresponding actions capable of setting Africa on course to a more sustainable future.'
+  }
+];
+
+const events = [
+  {
+    id: 'event-id',
+    title: '',
+    category: '',
+    event_date: '',
+    event_time: '',
+    description: '',
+    event_location: '',
+  }    
+];
+
+const talks = [
+  {
+    id: 'talk-id',
+    speaker_name: '',
+    topic: '',
+    video_url: '',
+    date: '',
+  }    
+];
+
+const blog = [];
+
+const partners = [
+  {
+    id: 'partner-id',
+    category: '',
+    partner_name: '',
+    thumbnail_url: '',
+  }  
+];
+
+const team = [
+  {
+    id: 'intro',
+    title: 'Our Team',
+    content: 'TEDxYaba’s core organizing committee is a group of passionate volunteers from a variety of disciplines who are dedicated to the TED mission of giving voice to ideas worth spreading.\n\nClick on the images to learn more about each team member.'
+  },
+  {
+    id: 'team-id',
+    name: '',
+    role: '',
+    image_url: '',
+    linkedin_url: '',
+    twitter_url: '',
+    bio: '',
+  }    
+];
+
+const socials = [
+  {
+    id: 'twitter',
+    url: 'https://twitter.com/tedxyaba',
+    icon_name: 'twitter',
+    font: 'Entypo'
+  },
+  {
+    id: 'instagram',
+    url: 'https://www.instagram.com/tedxyaba/',
+    icon_name: 'social-instagram',
+    font: 'SimpleLineIcons'
+  },
+  {
+    id: 'facebook',
+    url: 'https://web.facebook.com/TEDxYaba/',
+    icon_name: 'facebook',
+    font: 'Entypo'
+  },
+];
+
+export default {
+  about,
+  events,
+  talks,
+  blog,
+  partners,
+  team,
+  socials,
+}
