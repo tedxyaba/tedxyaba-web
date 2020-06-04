d7fa6c7da323be0ba4ad76c7b6323217e6bcb13a
Sunday Adefila <adefilaedward@gmail.com>
2020-06-04 09:24:38 +0100

    highlights

--- src/pages/LiveStream/LiveStream.jsx
+++ src/pages/LiveStream/LiveStream.jsx
@@ -25,43 +25,42 @@ const LiveStream = () => {
           <p>CONVERGENCE is the 2020 edition of TEDxYaba, an independently organized TED event inspired by its strong roots in Yaba, Lagos -  the centre of Africa’s largest technology ecosystem. In a discourse about Africa’s future, this conference is bringing together some of the finest thinkers and innovators involved in creating impact within Africa to spotlight on how technology can help Africa take advantage of its multi-diversity for better collaboration.</p>
         </div>
         <p><b>You can <a href={Brochure} download>click here to download</a> the digital brochure for the event.</b></p>
-        <h4 className='pt-3'>Schedule for the day:</h4>
+        <h4 className='pt-3'>Main Session Video Highlights:</h4>
         <hr align='left' className='mt-0' />
 
         <ul>
-          <li>12:00PM - 12:15PM WAT - Opening</li>
-          <li>
-            12:15PM - 1:35PM WAT - Session 1
-            <ul>
-              <li>Speaker 1 &#8594; Dr Lawal Bakare</li>
-              <li>Speaker 2 &#8594; Bright Simons</li>
-              <li>Speaker 3 &#8594; Astrid Haas</li>
-              <li>Breakout session
-                <ul>
-                  <li><a href='https://tedxyaba.zoom.us/j/98997517428' target='_blank'>Breakout room 1</a> &#8594; Fighting off Pandemics: Community Defense vs Herd Immunity (Dr Lawal Bakare)</li>
-                  <li><a href='https://tedxyaba.zoom.us/j/94687921485' target='_blank'>Breakout room 2</a> &#8594; Trust and entrepreneurship in Africa (Bright Simons)</li>
-                  <li><a href='https://tedxyaba.zoom.us/j/93576740090' target='_blank'>Breakout room 3</a> &#8594; Do Rural spaces need to continue to shrink for Urbanisation? (Astrid Haas)</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            1:35PM - 2:55PM WAT - Session 2
-            <ul>
-              <li>Speaker 4 &#8594; Andrew Nevin</li>
-              <li>Speaker 5 &#8594; Dr Charles Omole (<a href={TalkHighlights} download>download highlights</a>)</li>
-              <li>Speaker 6 &#8594; Lola Omolola (interviewed by Abosede George-Ogan)</li>
-              <li>Breakout session
-                <ul>
-                  <li><a href='https://tedxyaba.zoom.us/j/98125039425' target='_blank'>Breakout room 4</a> &#8594; First there were MDGs, now there are SDGs. What’s next? (Andrew S. Nevin)</li>
-                  <li><a href='https://tedxyaba.zoom.us/j/93647575804' target='_blank'>Breakout room 5</a> &#8594; Is a Pan African Security commission a myth? (Dr Charles Omole)</li>
-                  <li><a href='https://tedxyaba.zoom.us/j/97965166920' target='_blank'>Breakout room 6</a> &#8594; The FAQs in building sustainable communities (Lola Omolola)</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>2:55PM - 3:00PM WAT - Closing</li>
+          <li>0:00:00 &#8594; Intro Video</li>
+          <li>0:01:00 &#8594; TEDx Intro</li>
+          <li>0:02:23 &#8594; Message from Morgan Freeman</li>
+          <li>0:04:30 &#8594; TEDxYaba Story/About Convergence</li>
+          <li>0:06:42 &#8594; Welcome/Housekeeping</li>
+          <li>0:11:13 &#8594; Artists Introduction/Act 1</li>
+          <li>0:15:28 &#8594; Speaker 1 (Dr. Lawal Bakare) - Communities: Foundation of Health Security</li>
+          <li>0:30:36 &#8594; Act 2</li>
+          <li>0:32:27 &#8594; Speaker 2 (Bright Simons) - The Next Viral Apocalypse</li>
+          <li>0:47:18 &#8594; Act 3</li>
+          <li>0:49:33 &#8594; Speaker 3 (Astrid Haas) - Achieving the Promise of Cities that Work</li>
+          <li>1:03:51 &#8594; Break</li>
+          <li>1:08:23 &#8594; Act 4</li>
+          <li>1:10:24 &#8594; Speaker 4 (Andrew S. Nevin, PhD) - Shifting from a GDP Lens to the SDG Lens</li>
+          <li>1:22:55 &#8594; Act 5</li>
+          <li>1:27:02 &#8594; Speaker 5 (Dr. Charles Omole) - Promoting a Pan-African Security Cooperation & Convergence; <a href={TalkHighlights} download>download highlights</a></li>
+          <li>1:40:22 &#8594; Act 6</li>
+          <li>1:42:12 &#8594; Speaker 6 (Lola Omolola) - Interview on the Power of Community in Transforming Africa</li>
+          <li>2:11:43 &#8594; Break</li>
+          <li>2:12:06 &#8594; Closing/Vote of Thanks</li>
+          <li>2:21:07 &#8594; Arts Compilation</li>
         </ul>
+
+        <h4 className='pt-3'>Other TED Talks played during the event:</h4>
+        <hr align='left' className='mt-0' />
+
+        <ul>
+          <li>How augmented reality is changing activism <a href='https://www.ted.com/talks/glenn_cantave_how_augmented_reality_is_changing_activism' target='_blank'>on ted.com</a></li>
+          <li>The symbols of systemic racism -- and how to take away their power <a href='https://www.ted.com/talks/paul_rucker_the_symbols_of_systemic_racism_and_how_to_take_away_their_power' target='_blank'>on ted.com</a></li>
+          <li>The danger of silence <a href='https://www.ted.com/talks/clint_smith_the_danger_of_silence' target='_blank'>on ted.com</a></li>
+        </ul>
+
 
         <div className="sub-section sponsors-list">
           <h4 className="pt-3">Sponsors</h4>
